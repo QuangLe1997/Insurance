9d519cf35fd94e879f6aec317be31151c4bf4478
quang.le <quang.le@vieon.vn>
2022-04-10 17:22:04 +0700

    #fix deploy history router

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-const express = require("express");
-const path = require("path");
-const serveStatic = require("serve-static");
+var history = require('connect-history-api-fallback');
+var express = require("express");
+var path = require("path");
+var serveStatic = require("serve-static");
 app = express();
+app.use(history());
 app.use(serveStatic(__dirname + "/dist"));
-const port = process.env.PORT || 5000;
+var port = process.env.PORT || 5000;
 app.listen(port);
 console.log("server started " + port);
